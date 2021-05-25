076b7c301be8b8b689d82c2fa69d1f132c9628dd
jsh <tmdgusya@naver.com>
2021-05-25 23:19:20 +0900

    study: Reducer

--- func-component/src/App.js
+++ func-component/src/App.js
@@ -1,10 +1,12 @@
 const { Profile } = require("./Profile");
 const { UseEffectTest } = require("./useEffectTest");
+const { Users } = require("./TestUseReducer");
 
 function App() {
   return (
     <div className="App">
       <UseEffectTest />
+      <Users />
     </div>
   );
 }
--- func-component/src/TestUseReducer.js
+++ func-component/src/TestUseReducer.js
@@ -27,43 +27,51 @@ function reducer(state, action) {
   }
 }
 
-function Users() {
+export function Users() {
   const [state, dispatch] = useReducer(reducer, {
     loading: false,
     data: null,
     error: null,
   });
+
+  const fetchUsers = async () => {
+    dispatch({ type: "LOADING" }); // rerender trigger => 내부에 useState
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/users" // rerender trigger => 내부에 useState
+      );
+      dispatch({ type: "SUCCESS", data: response.data });
+    } catch (e) {
+      console.log(e);
+      dispatch({ type: "Error", error: e });
+    }
+  };
+
+  // 이러면 감지하는 값이 없어서 처음 mount 시에만 발동함
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const { loading, data: users, error } = state;
+
+  if (loading) return <div>로딩중...</div>;
+  if (error) return <div>에러가 발생했습니다.</div>;
+  if (!users) return null;
+
+  console.log(users);
+
+  return (
+    <div>
+      <ul>
+        {users.map((user) => {
+          return (
+            <li key={user.id}>
+              {user.username} ({user.name})
+            </li>
+          );
+        })}
+      </ul>
+      <button onClick={fetchUsers}>다시 불러오기</button>
+    </div>
+  );
 }
-
-const fetchUsers = async () => {
-  dispatch({ type: "LOADING" }); // rerender trigger => 내부에 useState
-  try {
-    const response = await axios.get(
-      "https://jsonplaceholder.typicode.com/users" // rerender trigger => 내부에 useState
-    );
-    dispatch({ type: "SUCCESS", data: response.data });
-  } catch (e) {
-    dispatch({ type: "Error", error: e });
-  }
-};
-
-useEffect(() => {
-  fetchUsers();
-}, []);
-
-const { loading, data: users, error } = state;
-
-if (loading) return <div>로딩중...</div>;
-if (error) return <div>에러가 발생했습니다.</div>;
-if (!users) return null;
-
-return (
-  <>
-    <ul>
-      {users.map((user) => {
-        <li key={user.id}>{user.username} ((user.name))</li>;
-      })}
-    </ul>
-    <button onClick={fetchUsers}></button>
-  </>
-);
